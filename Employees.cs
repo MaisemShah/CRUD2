289bc30022114a3fec01f9abd9a77c621e960ae4
MaisemShah <54338906+MaisemShah@users.noreply.github.com>
2019-10-08 18:43:01 -0500

    Add files via upload

---
+++ Employees.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace CRUD2.Data
+{
+    public class Employees
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EID { get; set; }
+        public string FN { get; set; }
+        public string LN { get; set; }
+
+        [ForeignKey("Did")]
+        public Departments DeptId { get; set; }
+
+
+    }
+}
